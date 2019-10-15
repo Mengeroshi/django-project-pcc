d15064ceeac6e3f0c574176bce9e3b875e33b11c
mengeroshi <dpllgr@hotmail.com>
2019-10-15 11:56:19 -0500

    topics template added

---
+++ learnin_logs/urls.py
@@ -0,0 +1,14 @@
+"""
+Define  URL pattern for learning_logs
+"""
+
+from django.urls import path
+from . import views
+
+app_name = 'learnin_logs'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    #Page that show all the topics
+    path('topics/', views.topics, name='topics'),
+]
