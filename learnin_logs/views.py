d4a01e2c6617ccd41ce32a102f4fd91755cea6b6
mengeroshi <dpllgr@hotmail.com>
2019-10-16 12:39:07 -0500

    add topic and entry added

--- learnin_logs/views.py
+++ learnin_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -18,3 +19,36 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_adedd')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learnin_logs/topic.html', context)
+
+def new_topic(request):
+    """Add new topic"""
+    if request.method != 'POST':
+        #NO DATA SUBMMITED; CREATE A BLANK FORM
+        form = TopicForm()
+    else:
+        #POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learnin_logs:topics')
+    
+    #Display a blank or invalid form.
+    context = {'form': form }
+    return render(request, 'learnin_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learnin_logs:topic', topic_id=topic_id)
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learnin_logs/new_entry.html', context)
