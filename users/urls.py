a2f1e660bfccb48798e852310d4a1bd254b6005b
mengeroshi <dpllgr@hotmail.com>
2019-10-26 22:31:51 -0500

    register user added

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 """ Defines URL patterns for users """
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 
 urlpatterns =[
     path('', include('django.contrib.auth.urls')),
+    #Registration Page
+    path('register/', views.register, name='register')
 ]
