cbaa523f5e6f950724c5cc78b7137b39fbf3683f
mengeroshi <dpllgr@hotmail.com>
2019-10-28 00:11:39 -0600

    Added custm 404 an 500 error pages 3

--- learnin_logs/views.py
+++ learnin_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
